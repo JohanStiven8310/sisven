89063af96cb1035db98fc8bce257eabf63593567
Johan Quiñonez <jhanstivn93@gmail.com>
2024-05-21 23:33:59 -0500

    route comuna api

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas');
+
