c4239eab3394a3e6417cb37d66e40876251ec941
Johan Quiñonez <jhanstivn93@gmail.com>
2024-05-21 23:42:41 -0500

    Routes de : customer, categories, paymodes, products

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,8 @@
 <?php
+use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\PayModeController;
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\ComunaController;
 use App\Http\Controllers\Api\MunicipioController;
 use Illuminate\Http\Request;
@@ -10,3 +14,7 @@ Route::get('/user', function (Request $request) {
 Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas');
 Route::apiResource('comunas', ComunaController::class);
 Route::apiResource('municipios', MunicipioController::class);
+Route::apiResource('customers', CustomerController::class);
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('paymodes', PayModeController::class);
+Route::apiResource('products', ProductController::class);
